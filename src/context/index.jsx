cf5b52de92ba0e8d5e47ca2256f25285c024c944
slince-zero <1711257704@qq.com>
2024-04-22 09:39:45 +0800

    完成费用计算组件

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,7 +1,45 @@
+/* eslint-disable react/prop-types */
 import { createContext } from 'react'
-
+import { useState } from 'react'
 export const GlobalContext = createContext()
 
 export default function GlobalSate({ children }) {
-  return <GlobalContext.Provider>{children}</GlobalContext.Provider>
+  const [formData, setFormData] = useState({
+    type: 'income',
+    amount: 0,
+    description: '',
+  })
+
+  const [value, setValue] = useState('expense')
+  const [totalExpense, setTotalExpense] = useState(0)
+  const [totalIncome, setTotalIncome] = useState(0)
+  const [allTransactions, setAllTransactions] = useState([])
+
+  function handleFormSubmit(currentFormData) {
+    if (!currentFormData.description || !currentFormData.amount) return
+
+    setAllTransactions([
+      ...allTransactions,
+      { ...currentFormData, id: Date.now() },
+    ])
+  }
+  return (
+    <GlobalContext.Provider
+      value={{
+        formData,
+        setFormData,
+        totalExpense,
+        setTotalExpense,
+        totalIncome,
+        setTotalIncome,
+        value,
+        setValue,
+        allTransactions,
+        setAllTransactions,
+        handleFormSubmit,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  )
 }
